738bd7159f06f3c93a2ef27853bb2fd362495257
Lucas Aster <asterlc102@gmail.com>
2022-11-19 00:14:53 -0300

    Aprendendo referencias e ponteiros

--- banco/main.go
+++ banco/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
@@ -12,19 +11,19 @@ func main() {
 		conta:   1132,
 		saldo:   5368.65,
 	}
-	conta.saque(2500)
-	fmt.Println(strconv.FormatFloat(conta.saldo, 'f', 4, 32))
 
-	conta.deposita(500)
-	fmt.Println(float32(conta.saldo))
+	conta2 := ContaCorrente{
+		titular: "Iris",
+		agencia: 11,
+		conta:   1562,
+		saldo:   3600,
+	}
 
-}
+	sucess, error := conta.transfere(557.75, &conta2)
+	fmt.Println(sucess, error)
+	fmt.Println(conta2.saldo)
+	fmt.Println(conta.saldo)
 
-type ContaPoupanca struct {
-	titular string
-	agencia int
-	conta   int
-	saldo   float64
 }
 
 type ContaCorrente struct {
@@ -32,6 +31,17 @@ type ContaCorrente struct {
 	agencia int
 	conta   int
 	saldo   float64
+}
+
+type RetornoTransacao struct {
+	message string
+	tipo    string
+	valor   float64
+}
+
+type ErrorTransacao struct {
+	reason string
+	code   string
 }
 
 func (c *ContaCorrente) saque(valor float64) string {
@@ -43,6 +53,36 @@ func (c *ContaCorrente) saque(valor float64) string {
 	}
 }
 
-func (c *ContaCorrente) deposita(valor float64) {
-	c.saldo += valor
+func (c *ContaCorrente) deposita(valor float64) (RetornoTransacao, ErrorTransacao) {
+	retval := new(RetornoTransacao)
+	erroval := new(ErrorTransacao)
+	if valor > 0 {
+		c.saldo += valor
+		retval.message = "Sucesso"
+		retval.tipo = "Deposito"
+		retval.valor = valor
+	} else {
+		erroval.reason = "Valor inválido"
+		erroval.code = "COD_104_INV"
+	}
+
+	return *retval, *erroval
+}
+
+func (c *ContaCorrente) transfere(valor float64, conta *ContaCorrente) (RetornoTransacao, ErrorTransacao) {
+	retval := new(RetornoTransacao)
+	erroval := new(ErrorTransacao)
+
+	if valor <= conta.saldo {
+		c.saldo -= valor
+		conta.saldo += valor
+		retval.message = "Sucesso"
+		retval.tipo = "Transferencia"
+		retval.valor = valor
+	} else {
+		erroval.reason = "Valor insuficiente"
+		erroval.code = "COD_116_NOFUND"
+	}
+
+	return *retval, *erroval
 }
